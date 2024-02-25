6c2d74c086f21836ddf412eac55fbe99cda999cb
skillerhero <rle16@hotmail.com>
2024-02-25 15:23:21 -0600

    abc

--- analisis/views/home.py
+++ analisis/views/home.py
@@ -1,4 +1,3 @@
-import functools
 from flask import render_template,Blueprint
 from analisis.models.user import User
 from analisis.models.muestra import Muestra
@@ -14,14 +13,19 @@ def get_user(id):
 
 @home.route("/")
 def index():
+    muestras=Muestra.query.all()
     descuentos=Descuento.query.all()
     analisis=Analisis.query.all()
     db.session.commit()
+
+    print("muestras index: ", muestras)
     return render_template('analistas/home.html',muestras=muestras,descuentos=descuentos,analisis=analisis)
 
 @home.route("/recepcion")
 def indexRecepcion():
+    muestras=Muestra.query.all()
     descuentos=Descuento.query.all()
     analisis=Analisis.query.all()
     db.session.commit()
+    print("muestras recepcion: ", muestras)
     return render_template('recepcion/home.html',muestras=muestras,descuentos=descuentos,analisis=analisis)
