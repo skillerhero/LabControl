f283cc6456230fc1b86bed12a1f2c655733c6042
yess1911 <yessenia.carbajal2864@alumnos.udg.mx>
2024-03-01 23:28:58 -0600

    Detalles muestras

--- analisis/views/recepcion.py
+++ analisis/views/recepcion.py
@@ -16,7 +16,7 @@ def get_user(id):
     return user
 
 @recepcion.route("/")
-def index():
+def home():
     muestras=Muestra.query.all()
     descuentos=Descuento.query.all()
     analisis=Analisis.query.all()
@@ -60,3 +60,27 @@ def registrarMuestra():
     descuentos=Descuento.query.all()
     analisis=Analisis.query.all()
     return render_template('analisis/registroMuestra.html',muestras=muestras,descuentos=descuentos,analisis=analisis, segment="registrarM")
+
+@recepcion.route('/detalle_muestra/<int:mues_id>', methods=['GET', 'POST'])
+def detalle_muestra(mues_id):
+    recepcion = Muestra.query.get_or_404(mues_id)
+    if request.method == 'POST':
+        recepcion.muestra_folio = request.form['mues_folio']
+        print(recepcion.muestra_folio)
+        recepcion.muestra_nombre = request.form['mues_nombre']
+        recepcion.muestra_apellido_paterno = request.form['mues_apellido_paterno']
+        recepcion.muestra_apellido_materno = request.form['mues_apellido_materno']
+        recepcion.muestra_telefono = request.form['mues_tel']
+        recepcion.muestra_email = request.form['mues_email']
+        recepcion.muestra_calle = request.form['mues_calle']
+        recepcion.muestra_colonia = request.form['mues_colonia']
+        recepcion.muestra_num_ext = request.form['mues_num_ext']
+        recepcion.muestra_num_int = request.form['mues_num_int']
+        recepcion.muestra_horas_ayuno = request.form['mues_horas_ayuno']
+        recepcion.muestra_alimentos = request.form['mues_alimentos']
+        recepcion.muestra_enfermedades = request.form['mues_enfermedades']
+        recepcion.muestra_medicamentos = request.form['mues_medicamentos']
+        recepcion.muestra_rubrica = request.form['mues_rubrica']
+        db.session.commit()
+        return redirect(url_for('recepcion.home'))
+    return render_template('recepcion/detalle_muestra.html', recepcion=recepcion, segment='detalle_muestra')
