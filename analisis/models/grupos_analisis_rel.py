2d499f7eec905aea67048a0636dfa2ab3cb8961b
skillerhero <rle16@hotmail.com>
2024-03-03 21:26:20 -0600

    correccion

--- analisis/models/grupos_analisis_rel.py
+++ analisis/models/grupos_analisis_rel.py
@@ -2,8 +2,8 @@ from analisis import db
 import datetime
 class GruposAnalisisRel(db.Model):
     __tablename__='grupos_analisis_rel'
-    gana_grupo_id_fk =db.Column(db.Integer)
-    gana_ana_id_fk =db.Column(db.Integer)
+    gana_grupo_id_fk =db.Column(db.Integer,primary_key=True)
+    gana_ana_id_fk =db.Column(db.Integer,primary_key=True)
 
 
     def __init__(self,gana_grupo_id_fk, gana_ana_id_fk )->None:
