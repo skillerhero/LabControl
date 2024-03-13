f6ab28e7c5247a1b445576ab24776936f1dc9bc9
RAFAEL LEON ESTRADA <rle16@hotmail.com>
2024-03-13 09:21:40 -0600

    cambios

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def index():
         return redirect(url_for("home.index"))
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=80)
+    socketio.run(app, host='0.0.0.0', port=5000)
     app.run()
