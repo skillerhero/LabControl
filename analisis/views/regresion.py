dddc135a2fe42a06280e34432be77d81b2969b7b
skillerhero <rle16@hotmail.com>
2024-03-12 19:21:02 -0600

    cambios

--- analisis/views/regresion.py
+++ analisis/views/regresion.py
@@ -4,17 +4,9 @@ from sklearn.linear_model import LinearRegression
 
 regresion = Blueprint('regresion', __name__, url_prefix='/regresion')
 
-materiales_a = np.array([[100, 150, 200, 250, 300],
-                         [110, 160, 210, 260, 310],
-                         [120, 170, 220, 270, 320]])
-
-materiales_b = np.array([[80, 120, 160, 200, 240],
-                         [85, 125, 165, 205, 245],
-                         [90, 130, 170, 210, 250]])
-
-materiales_c = np.array([[50, 75, 100, 125, 150],
-                         [55, 80, 105, 130, 155],
-                         [60, 85, 110, 135, 160]])
+materiales_a = np.load("materialesA.npy")
+materiales_b = np.load("materialesB.npy")
+materiales_c = np.load("materialesC.npy")
 
 modelo = LinearRegression()
 
@@ -26,15 +18,55 @@ def index():
 def predict():
     dia_semana = int(request.form['dia_semana'])
 
-    X = np.array([[1], [2], [3]])
+    listInputs=[]
+    listOutputs=[]
+    x = materiales_a[:,dia_semana]
+    valoresAnteriores=3
+    for i in range(valoresAnteriores,len(x)):
+        entrada=np.reshape(x[i-valoresAnteriores:i],-1)
+        listInputs.append(entrada)
+        listOutputs.append(x[i])
+    listInputs=np.asanyarray(listInputs)
+    listOutputs=np.asanyarray(listOutputs).ravel()
+    modelo.fit(listInputs,listOutputs)
 
-    modelo.fit(X, materiales_a[:, dia_semana-1].reshape(-1, 1))
-    prediccion_a = int(modelo.predict([[dia_semana]])[0])
+    prediccion_a = int(modelo.predict([materiales_a[-3:,dia_semana]])[0])
 
-    modelo.fit(X, materiales_b[:, dia_semana-1].reshape(-1, 1))
-    prediccion_b = int(modelo.predict([[dia_semana]])[0])
+    x = materiales_b[:,dia_semana]
+    valoresAnteriores=3
+    for i in range(valoresAnteriores,len(x)):
+        entrada=np.reshape(x[i-valoresAnteriores:i],-1)
+        listInputs.append(entrada)
+        listOutputs.append(x[i])
+    listInputs=np.asanyarray(listInputs)
+    listOutputs=np.asanyarray(listOutputs).ravel()
+    modelo.fit(listInputs,listOutputs)
 
-    modelo.fit(X, materiales_c[:, dia_semana-1].reshape(-1, 1))
-    prediccion_c = int(modelo.predict([[dia_semana]])[0])
+    prediccion_b = int(modelo.predict([materiales_b[-3:,dia_semana]])[0])
 
+    x = materiales_c[:,dia_semana]
+    valoresAnteriores=3
+    for i in range(valoresAnteriores,len(x)):
+        entrada=np.reshape(x[i-valoresAnteriores:i],-1)
+        listInputs.append(entrada)
+        listOutputs.append(x[i])
+    listInputs=np.asanyarray(listInputs)
+    listOutputs=np.asanyarray(listOutputs).ravel()
+    modelo.fit(listInputs,listOutputs)
+    prediccion_c = int(modelo.predict([materiales_c[-3:,dia_semana]])[0])
     return render_template('regresion/result.html', prediccion_a=prediccion_a, prediccion_b=prediccion_b, prediccion_c=prediccion_c)
+
+@regresion.route('/historico', methods=['GET', 'POST'])
+def historico():
+    # Cargar los datos
+    materialesa = np.load("materialesa.npy")
+    materialesb = np.load("materialesb.npy")
+    materialesc = np.load("materialesc.npy")
+
+    # Convertir los datos a listas para poder usarlos en la plantilla
+    lista_a = materialesa.tolist()
+    lista_b = materialesb.tolist()
+    lista_c = materialesc.tolist()
+
+    # Pasar los datos a la plantilla
+    return render_template('historico.html', lista_a=lista_a, lista_b=lista_b, lista_c=lista_c)
